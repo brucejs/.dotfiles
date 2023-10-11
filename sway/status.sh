48e48fa71968f10c5b817aeea46ef3a54d58ea5a
Bruce Stephenson <bruce@brucejs.com>
2023-10-10 21:13:13 -0500

    Simplify sway status command

--- sway/status.sh
+++ sway/status.sh
@@ -2,22 +2,8 @@
 set -euo pipefail
 IFS=$'\n\t'
 # icons: ☀️💡🔇🔈🔉🔊🔋🔌🕯️🪫
-BACKLIGHT_LEVEL=$(cat /sys/class/backlight/intel_backlight/brightness)
-BACKLIGHT_PERCENTAGE=$(brightnessctl -m | awk -F',' '{ print $4 }')
 BATTERY_PERCENTAGE=$(cat /sys/class/power_supply/BAT0/capacity)
 BATTERY_STATUS=$(cat /sys/class/power_supply/BAT0/status)
-DATE_FORMATTED=$(date +'%a %Y-%m-%d %H:%M')
-#VOLUME_LEVEL=$(wpctl get-volume @DEFAULT_AUDIO_SINK@)
-
-function set_backlight_icon {
-    if (($BACKLIGHT_LEVEL >= 512)); then
-        backlight_icon='☀️'
-    elif (($BACKLIGHT_LEVEL >= 128 && $BACKLIGHT_LEVEL < 512)); then
-        backlight_icon='💡'
-    else
-        backlight_icon='🕯️'
-    fi
-}
 
 function set_battery_icon {
     if [[ $BATTERY_STATUS != "Discharging" ]]; then
@@ -29,8 +15,6 @@ function set_battery_icon {
     fi
 }
 
-set_backlight_icon
-
 set_battery_icon
 
-echo "${BACKLIGHT_PERCENTAGE}${backlight_icon} | ${BATTERY_PERCENTAGE}%${battery_icon} | $DATE_FORMATTED"
+echo "${BATTERY_PERCENTAGE}%${battery_icon}"
