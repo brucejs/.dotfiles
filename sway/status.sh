380e596ad3b1e6505646cbd1eda5b9c65723eb3d
Bruce Stephenson <bruce@brucejs.com>
2023-10-11 13:07:21 -0500

    Re-add/refactor backlight level to sway status

--- sway/status.sh
+++ sway/status.sh
@@ -4,6 +4,9 @@ IFS=$'\n\t'
 # icons: ☀️💡🔇🔈🔉🔊🔋🔌🕯️🪫
 BATTERY_PERCENTAGE=$(cat /sys/class/power_supply/BAT0/capacity)
 BATTERY_STATUS=$(cat /sys/class/power_supply/BAT0/status)
+BRIGHTNESS_LEVEL=$(brightnessctl get)
+BRIGHTNESS_MAX=$(brightnessctl max)
+BRIGHTNESS_PERCENTAGE=$(awk "BEGIN { print ($BRIGHTNESS_LEVEL/$BRIGHTNESS_MAX) * 100 }")
 
 function set_battery_icon {
     if [[ $BATTERY_STATUS != "Discharging" ]]; then
@@ -15,6 +18,18 @@ function set_battery_icon {
     fi
 }
 
+function set_brightness_icon {
+    if [[ $BRIGHTNESS_PERCENTAGE -ge 80 ]]; then
+        brightness_icon='☀️'
+    elif [[ $BRIGHTNESS_PERCENTAGE -ge 25 ]]; then
+        brightness_icon='💡'
+    else
+        brightness_icon='🕯️'
+    fi
+}
+
 set_battery_icon
 
-echo "${BATTERY_PERCENTAGE}%${battery_icon}"
+set_brightness_icon
+
+echo "${BRIGHTNESS_PERCENTAGE}%${brightness_icon} | ${BATTERY_PERCENTAGE}%${battery_icon}"
