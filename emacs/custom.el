13c30ebb8b1ccfa72615c8680b978645c3f33f4b
Bruce Stephenson <bruce@brucejs.com>
2024-02-15 21:11:54 -0600

    Add minimalist Emacs config

---
+++ emacs/custom.el
@@ -0,0 +1,22 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(backup-directory-alist '(("." . "/tmp/")))
+ '(diary-file "~/Documents/Git/org/diary.txt")
+ '(fill-column 79)
+ '(org-agenda-files '("./tasks.org" "./calendar.org"))
+ '(org-agenda-include-diary t)
+ '(org-agenda-show-future-repeats nil)
+ '(org-deadline-warning-days 0)
+ '(org-default-notes-file "./inbox.org")
+ '(org-directory "~/Documents/Git/org")
+ '(org-hide-leading-stars t)
+ '(org-startup-folded 'content))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ emacs/init.el
@@ -0,0 +1,6 @@
+(setq custom-file "~/.config/emacs/custom.el")
+(load custom-file)
+
+(global-set-key (kbd "C-c a") 'org-agenda)
+(global-set-key (kbd "C-c c") 'org-capture)
+(global-set-key (kbd "C-c l") 'org-store-link)
